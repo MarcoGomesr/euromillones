0aaef81d7150a36256c8130e89adec883291060d
marco gomes <marcogomesr@gmail.com>
2025-05-05 20:09:54 +0200

    Refactor fetch options in getEuroMillionResults for clarity
    - Consolidate fetch options into a single object for improved readability.
    - Maintain existing functionality while enhancing code structure.

--- src/services/EuroMillion.ts
+++ src/services/EuroMillion.ts
@@ -1,13 +1,14 @@
 export const getEuroMillionResults = async () => {
-  const options = {
-    method: 'GET',
-    headers: { accept: 'application/json' },
-    next: { revalidate: 60 }
-  }
-
   const currentYear = new Date().getFullYear()
   const API_URL = `https://euromillions.api.pedromealha.dev/draws?year=${currentYear}`
-  const res = await fetch(API_URL, options)
+
+  const res = await fetch(API_URL, {
+    method: 'GET',
+    headers: {
+      accept: 'application/json'
+    },
+    next: { revalidate: 60 }
+  })
 
   if (!res.ok) return []
 
